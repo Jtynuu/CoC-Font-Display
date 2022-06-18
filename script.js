b94e99785c9974c4e316ac48782670ef096fb4f1
Jtynuu <86546730+Jtynuu@users.noreply.github.com>
2022-06-18 14:24:46 +0700

    Update - script.js

--- script.js
+++ script.js
@@ -4,6 +4,14 @@ function main() {
   const input = window.prompt("Tolong isi kata kata yang ingin ditampilkan.");
   
   // Perubahan jika user selesai input
-  document.getElementById("keterangan").innerHTML = `Anda telah memasukkan kata "${input}", dan ditampilkan sebagai berikut...`
+  document.getElementById("keterangan").innerHTML = `Anda telah memasukkan kata "${input}", dan ditampilkan sebagai berikut...`;
   document.getElementById("hasil").innerHTML = input;
+  
+  if(input == null) {
+    alert("Wadoo! kenapa di cancel pak?");
+    document.getElementById("keterangan").innerHTML = `Anda telah membatalkan sesi form, ada apa?`;
+  } else if(input == "") {
+    alert("Tolong diisi fieldnya pak!");
+    document.getElementById("keterangan").innerHTML = `Anda tidak memasukkan apa-apa.`;
+  }
 }
